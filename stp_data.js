d6446a2f93e91b6d744b69bdd194733df3434cb5
SkillTroop <127951331+00003142660@users.noreply.github.com>
2026-04-22 12:43:39 +0530

    Add files via upload

---
+++ stp_data.js
@@ -0,0 +1,69 @@
+const stpData = [
+  {
+    "S. No.": 1,
+    "STP": "Okhla Phase VI",
+    "Latitude": 28.5490059,
+    "Longitude": 77.27796516,
+    "Design capacity\n (in MGD)": 30,
+    "Utilisation \n(in MGD)": 29,
+    "% Utilisation": 97,
+    "Designed BOD/SS\n (in mg./lt.)": "20/30",
+    "Achieved BOD/SS\n (in mg./lt.)": "8/10",
+    "Whether Meeting Design\n Parameters or Not": "Yes",
+    "Use of  Treated Sewage": "Horticulture"
+  },
+  {
+    "S. No.": 2,
+    "STP": "Keshopur Phase III",
+    "Latitude": 28.6529931,
+    "Longitude": 77.08580228,
+    "Design capacity\n (in MGD)": 40,
+    "Utilisation \n(in MGD)": 38,
+    "% Utilisation": 95,
+    "Designed BOD/SS\n (in mg./lt.)": "30/50",
+    "Achieved BOD/SS\n (in mg./lt.)": "55.28/129.37",
+    "Whether Meeting Design\n Parameters or Not": "No",
+    "Use of  Treated Sewage": "PPCL Power Plant"
+  },
+  {
+    "S. No.": 3,
+    "STP": "Coronation Pillar Phase III",
+    "Latitude": 28.72647111,
+    "Longitude": 77.18674324,
+    "Design capacity\n (in MGD)": 10,
+    "Utilisation \n(in MGD)": 7,
+    "% Utilisation": 70,
+    "Designed BOD/SS\n (in mg./lt.)": "30/50",
+    "Achieved BOD/SS\n (in mg./lt.)": "23.98/33.78",
+    "Whether Meeting Design\n Parameters or Not": "Yes",
+    "Use of  Treated Sewage": "In-house use"
+  },
+  {
+    "S. No.": 4,
+    "STP": "Rithala Phase II",
+    "Latitude": 28.72479383,
+    "Longitude": 77.10500221,
+    "Design capacity\n (in MGD)": 40,
+    "Utilisation \n(in MGD)": 40,
+    "% Utilisation": 100,
+    "Designed BOD/SS\n (in mg./lt.)": "15/20",
+    "Achieved BOD/SS\n (in mg./lt.)": "12.62/17.39",
+    "Whether Meeting Design\n Parameters or Not": "Yes",
+    "Use of  Treated Sewage": "Horticulture"
+  },
+  {
+    "S. No.": 5,
+    "STP": "Pappankalan Phase II",
+    "Latitude": 28.58799988,
+    "Longitude": 77.02539301,
+    "Design capacity\n (in MGD)": 20,
+    "Utilisation \n(in MGD)": 19.5,
+    "% Utilisation": 97.5,
+    "Designed BOD/SS\n (in mg./lt.)": "10/10",
+    "Achieved BOD/SS\n (in mg./lt.)": "4.43/6.01",
+    "Whether Meeting Design\n Parameters or Not": "Yes",
+    "Use of  Treated Sewage": "Horticulture"
+  }
+];
+
+initMap();
